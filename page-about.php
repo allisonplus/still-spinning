84d55b0cca36b5352126bb731d91501902f10614
Allison Tarr <allison@webdevstudios.com>
2016-09-05 18:24:13 -0400

    add separate template part for blog, add home.php about about template

---
+++ page-about.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying the About Page.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Spin
+ */
+
+get_header();  ?>
+
+<div class="main">
+	<div class="container">
+
+	<div class="page-content-about">
+
+		<div class="user-photo">
+			<?php $image = get_field('user-image'); ?>
+			<img class="photo" src="<?php echo $image['sizes']['highlighted'] ?>">
+		</div> <!--/.user-photo-->
+
+		<div class="info">
+			<?php // Start the loop ?>
+			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+			  <?php the_content(); ?>
+
+			<?php endwhile; // end the loop?>
+		</div> <!--/.info-->
+
+	</div> <!-- /.page-content-about -->
+
+	</div> <!-- /.container -->
+</div> <!-- /.main -->
+
+<?php get_footer(); ?>
