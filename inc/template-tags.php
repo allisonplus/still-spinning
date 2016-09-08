c63aec796a176b73024c664a16e82a5119c30a26
Allison Tarr <allison@webdevstudios.com>
2016-09-07 20:03:01 -0400

    convert cps_get_custom_loop_random to be a bit more manageable

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -467,29 +467,22 @@ function cps_get_custom_loop_random() {
 
 	// Post query.
 	query_posts(array(
-		'showposts' => 6,
-		'orderby' => 'rand',
+		'showposts'     => 6,
+		'orderby'       => 'rand',
 		'category_name' => 'feminism' // You can insert any category name
 	));
 
-	// // Open a list ( <ul> ).
-	$return_string = '<ul class="random-posts">';
-
-	// Creating the list of posts.
-	if (have_posts()) :
-		while (have_posts()) : the_post();
-			$return_string .= '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
-		endwhile;
-	endif;
-	// Close the list.
-	$return_string .= '</ul>';
-
-	// Resetting the WP query.
-	wp_reset_query();
-	// Displaying the list of random posts.
-	return $return_string;
-
 	ob_start(); ?>
+
+	<ul class="random-posts">
+
+	<?php if (have_posts()) : ?>
+		<?php while (have_posts()) : the_post(); ?>
+			<li><a href="<?php echo get_permalink(); ?>"><?php esc_html_e( the_title() ); ?></a></li>
+		<?php endwhile; ?>
+	<?php endif; ?>
+	<?php wp_reset_query(); ?>
+	</ul>
 
 	<?php
 	return ob_get_clean();
