35ba31c1dc3bdbfbd346813c387119234c9dee85
Allison Tarr <allison@webdevstudios.com>
2016-09-18 21:56:38 -0400

    add wrap

--- page-about.php
+++ page-about.php
@@ -10,27 +10,27 @@
 get_header();  ?>
 
 <div class="main">
-	<div class="container">
+	<div class="wrap">
 
-	<div class="page-content-about">
+		<div class="page-content-about">
 
-		<div class="user-photo">
-			<?php $image = get_field('user-image'); ?>
-			<img class="photo" src="<?php echo $image['sizes']['highlighted'] ?>">
-		</div> <!--/.user-photo-->
+			<div class="user-photo">
+				<?php $image = get_field('user-image'); ?>
+				<img class="photo" src="<?php echo $image['sizes']['highlighted'] ?>">
+			</div> <!--/.user-photo-->
 
-		<div class="info">
-			<?php // Start the loop ?>
-			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+			<div class="info">
+				<?php // Start the loop ?>
+				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-			  <?php the_content(); ?>
+					<?php the_content(); ?>
 
-			<?php endwhile; // end the loop?>
-		</div> <!--/.info-->
+				<?php endwhile; // end the loop?>
+			</div> <!--/.info-->
 
-	</div> <!-- /.page-content-about -->
+		</div> <!-- /.page-content-about -->
 
-	</div> <!-- /.container -->
+	</div> <!-- /.wrap -->
 </div> <!-- /.main -->
 
 <?php get_footer(); ?>
