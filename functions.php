1c77a5241c4feb0bd22f553fc6ffea82909dfa19
Allison Tarr <allison@webdevstudios.com>
2017-02-20 14:09:18 -0500

    add featured-blog image size

--- functions.php
+++ functions.php
@@ -8,71 +8,74 @@
  */
 
 if ( ! function_exists( 'cps_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function cps_setup() {
 	/**
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on Spin, use a find and replace
-	 * to change 'cps' to the name of your theme in all the template files.
-	 * You will also need to update the Gulpfile with the new text domain
-	 * and matching destination POT file.
-	 */
-	load_theme_textdomain( 'cps', get_template_directory() . '/languages' );
-
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
-
-	/**
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
-	add_theme_support( 'title-tag' );
-
-	/**
-	 * Enable support for Post Thumbnails on posts and pages.
+	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
 	 */
-	add_theme_support( 'post-thumbnails' );
+	function cps_setup() {
+		/**
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on Spin, use a find and replace
+		 * to change 'cps' to the name of your theme in all the template files.
+		 * You will also need to update the Gulpfile with the new text domain
+		 * and matching destination POT file.
+		 */
+		load_theme_textdomain( 'cps', get_template_directory() . '/languages' );
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => esc_html__( 'Primary Menu', 'cps' ),
-		'mobile'  => esc_html__( 'Optional Mobile Menu', 'cps' ),
-	) );
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
 
-	/**
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
-	) );
+		/**
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'cps_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
+		/**
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
 
-	// Add styles to the post editor
-	add_editor_style( array( 'editor-style.css', cps_font_url() ) );
+		// Featured Blog images.
+		add_image_size( 'featured-blog', 450, 321, array( 'center', 'center' ) );
 
-}
-endif; // cps_setup
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menus( array(
+			'primary' => esc_html__( 'Primary Menu', 'cps' ),
+			'mobile'  => esc_html__( 'Optional Mobile Menu', 'cps' ),
+		) );
+
+		/**
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		) );
+
+		// Set up the WordPress core custom background feature.
+		add_theme_support( 'custom-background', apply_filters( 'cps_custom_background_args', array(
+			'default-color' => 'ffffff',
+			'default-image' => '',
+		) ) );
+
+		// Add styles to the post editor.
+		add_editor_style( array( 'editor-style.css', cps_font_url() ) );
+
+	}
+endif; // cps_setup.
 add_action( 'after_setup_theme', 'cps_setup' );
 
 /**
@@ -94,19 +97,18 @@ add_action( 'after_setup_theme', 'cps_content_width', 0 );
  */
 function cps_widgets_init() {
 
-	// Define sidebars
+	// Define sidebars.
 	$sidebars = array(
 		'sidebar-1'  => esc_html__( 'Sidebar 1', 'cps' ),
 		'sidebar-2'  => esc_html__( 'Footer Widgets', 'cps' ),
-	//	'sidebar-3'  => esc_html__( 'Sidebar 3', 'cps' ),
 	);
 
-	// Loop through each sidebar and register
+	// Loop through each sidebar and register.
 	foreach ( $sidebars as $sidebar_id => $sidebar_name ) {
 		register_sidebar( array(
 			'name'          => $sidebar_name,
 			'id'            => $sidebar_id,
-			'description'   => sprintf ( esc_html__( 'Widget area for %s', 'cps' ), $sidebar_name ),
+			'description'   => sprintf( esc_html__( 'Widget area for %s', 'cps' ), $sidebar_name ),
 			'before_widget' => '<aside class="widget %2$s">',
 			'after_widget'  => '</aside>',
 			'before_title'  => '<h3 class="widget-title">',
