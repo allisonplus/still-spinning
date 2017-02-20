b69302a03154b5fad3e1653a6cfde228a51c3083
Allison Tarr <allison@webdevstudios.com>
2017-02-20 15:58:35 -0500

    adjust header + navigation styles to coordinate more + change markup organization

--- header.php
+++ header.php
@@ -28,30 +28,18 @@
 	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'cps' ); ?></a>
 
 	<header class="site-header">
+		<h1 class="site-title"><a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 
-			<div class="site-branding">
-				<?php if ( is_front_page() && is_home() ) : ?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php else : ?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php endif;
-
-				$description = get_bloginfo( 'description', 'display' );
-				if ( $description || is_customize_preview() ) : ?>
-					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-				<?php endif; ?>
-			</div><!-- .site-branding -->
-
-			<nav id="site-navigation" class="main-navigation">
-				<?php
-					wp_nav_menu( array(
-						'theme_location' => 'primary',
-						'container'      => 'false',
-						'menu_id'        => 'primary-menu',
-						'menu_class'     => 'menu dropdown',
-					) );
-				?>
-			</nav><!-- #site-navigation -->
+		<nav id="site-navigation" class="main-navigation">
+			<?php
+				wp_nav_menu( array(
+					'container'      => 'false',
+					'theme_location' => 'primary',
+					'menu_id'        => 'primary-menu',
+					'menu_class'     => 'menu dropdown',
+				) );
+			?>
+		</nav><!-- #site-navigation -->
 
 	</header><!-- #masthead -->
 
