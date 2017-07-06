710a81a79b9f303994b5d375b50e115adeb51229
Allison Tarr <allison@webdevstudios.com>
2017-07-06 18:15:43 -0400

    rehaul markup on about page

--- page-about.php
+++ page-about.php
@@ -7,28 +7,27 @@
  * @package Spin
  */
 
-get_header();  ?>
+get_header(); ?>
 
-<div class="main">
 	<div class="wrap">
+		<div class="primary content-area">
+			<main id="main" class="site-main" role="main">
 
-		<div class="page-content-about">
+				<div class="user-photo">
+					<?php $image = get_field( 'user-image' ); ?>
+					<img class="photo" src="<?php echo esc_url( $image['sizes']['medium_large'] ); ?>">
+				</div> <!--/.user-photo-->
 
-			<div class="user-photo">
-				<?php $image = get_field( 'user-image' ); ?>
-				<img class="photo" src="<?php echo esc_url( $image['sizes']['medium_large'] ); ?>">
-			</div> <!--/.user-photo-->
+				<div class="info">
+					<?php
+					while ( have_posts() ) : the_post();
+						the_content();
+					endwhile; ?>
+				</div> <!--/.info-->
 
-			<div class="info">
-				<?php
-				while ( have_posts() ) : the_post();
-					the_content();
-				endwhile; ?>
-			</div> <!--/.info-->
+			</main><!-- #main -->
+		</div><!-- .primary -->
 
-		</div> <!-- /.page-content-about -->
-
-	</div> <!-- /.wrap -->
-</div> <!-- /.main -->
+	</div><!-- .wrap -->
 
 <?php get_footer(); ?>
