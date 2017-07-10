325817eddeecdfb652b2b37572afa682f7adbb8d
Allison Tarr <hello@allisontarr.com>
2017-07-10 16:18:29 -0400

    create new query for archive posts

--- inc/queries.php
+++ inc/queries.php
@@ -20,3 +20,24 @@ function cps_get_recent_posts() {
 		'update_post_term_cache' => false,
 	) );
 }
+
+/**
+ * Get 6 posts from the relevant category.
+ *
+ * @param  int|string $cat_id The category ID for getting posts.
+ * @return WP_Query The category/archive posts.
+ */
+function cps_get_archive_posts( $cat_id ) {
+
+	$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+
+	return new WP_Query( array(
+		'post_type'              => 'post',
+		'cat'                    => absint( $cat_id ),
+		'posts_per_page'         => 6,
+		'no_found_rows'          => true,
+		'update_post_meta_cache' => false,
+		'update_post_term_cache' => false,
+		'paged'                  => $paged,
+	) );
+}
